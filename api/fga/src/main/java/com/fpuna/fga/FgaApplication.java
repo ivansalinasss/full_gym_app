d43e07bacf07e51ab001bc7c35be63adcdd3dee9
Ivan Salinas <contactoivansalinasss@gmail.com>
2022-09-26 22:56:47 -0400

    Se agrega la API

---
+++ api/fga/src/main/java/com/fpuna/fga/FgaApplication.java
@@ -0,0 +1,13 @@
+package com.fpuna.fga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgaApplication.class, args);
+	}
+
+}
---
+++ api/fga/src/test/java/com/fpuna/fga/FgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpuna.fga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
